daefd094326ca82a3a8ec3e1e97bea9075b0d33c
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 23:12:47 +0530

    Done with Program 22

---
+++ src/fundamentals/Program_22.java
@@ -0,0 +1,26 @@
+package fundamentals;
+
+import java.util.Scanner;
+
+public class Program_22 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Name : ");
+		String name = sc.nextLine();
+		System.out.print("Age : ");
+		int age = sc.nextInt();
+		System.out.print("Gender : ");
+		char gender = sc.next().charAt(0);
+		
+		System.out.println("<--Output-->");
+		System.out.println("Name   : "+name);
+		System.out.println("Age    : "+age);
+		System.out.println("Gender : "+gender);
+		
+		sc.close();
+
+	}
+
+}
