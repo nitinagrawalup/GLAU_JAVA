a3e94e6ddbaafa057939f0a1dec46479bf49e952
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 19:38:54 +0530

    Done with Program 8

---
+++ src/fundamentals/Program_8.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_8 {
+
+	public static void main(String[] args) {
+		
+		double result ;
+		result=  2 + 3 - 5 *2/5;
+		System.out.println("2 + 3 – 5 *2 /5 = "+ result);
+	}
+
+}
+/*
+WAP to solve the following expression:
+2 + 3 – 5 *2 /5; store the result in a variable and print it on the output
+screen.
+*/
