37bb68185dd54c3b0c7011206e43730ee956d524
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 20:14:21 +0530

    Done with Program 13

---
+++ src/fundamentals/Program_13.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_13 {
+
+	public static void main(String[] args) {
+		
+		int result = -2 % -5;
+		System.out.println("-2 % -5 = "+ result);
+
+	}
+
+}
+
+/*
+WAP to solve the expression :
+-2 % -5 ; store the result in a variable and print it on the output screen
+*/
