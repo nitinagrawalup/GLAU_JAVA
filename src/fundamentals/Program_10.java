ed7bcce79b39926372a9105a6bbe4d40bc5b36d7
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 19:48:58 +0530

    Done with the Program 10

---
+++ src/fundamentals/Program_10.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+public class Program_10 {
+
+	public static void main(String[] args) {
+		
+		int result;
+		result = 23 % 10;
+		System.out.println(" 23 % 10 = " + result);
+
+	}
+
+}
+
+/*
+WAP to solve the expression :
+23 % 10 ; store the result in a variable and print it on the output screen.
+*/
