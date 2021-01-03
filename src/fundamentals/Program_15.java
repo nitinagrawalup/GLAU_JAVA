5d92488dca450bc1bbd06fc0fa932aac3feef854
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 20:18:09 +0530

    Done with Program 15

---
+++ src/fundamentals/Program_15.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_15 {
+
+	public static void main(String[] args) {
+		
+		double result = 23 / 10 ;
+		System.out.println("23 / 10 = "+result);
+
+	}
+
+}
+
+/*
+WAP to solve the expression :
+23 / 10 ; store the result in a variable and print it on the output screen.
+*/
