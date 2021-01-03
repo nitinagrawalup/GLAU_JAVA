a46a35cd3ad726c844e121ba4045bba2e4dea6e0
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 18:57:17 +0530

    Done with Program 6

---
+++ src/fundamentals/Program_6.java
@@ -0,0 +1,25 @@
+package fundamentals;
+
+public class Program_6 {
+
+	public static void main(String[] args) {
+		
+		int input1, input2;
+		input1=20;
+		input2=30;
+		
+		System.out.println("input1 = "+input1 +";");
+		System.out.println("input2 = "+input2 +";");
+
+	}
+
+}
+
+/*
+WAP to declare and initialize two variable of type integer and print
+there value on the output screen.
+Hint : int input1 = 20;
+	   int input2 = 30;
+Output: input1 = 20;
+ 		input2 = 30;
+*/
