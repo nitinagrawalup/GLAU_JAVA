8396cccb573421584ff4029a53684c457bca265a
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 18:30:36 +0530

    Done with Program 1

---
+++ src/fundamentals/Program_1.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java 12");
+
+	}
+
+}
+
+/*
+WAP to display “Welcome to Java 12” on the output screen.
+Output : Welcome to Java 12
+*/
+
