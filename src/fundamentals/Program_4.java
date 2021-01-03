84883d87ca725f22a350157c1416602baeafa9d7
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 18:47:02 +0530

    Done with Program 4

---
+++ src/fundamentals/Program_4.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world!!!!");
+
+	}
+
+}
+/*
+
+WAP to print “hello world!!!!” on the output screen.
+Output: hello world!!!!
+
+*/
