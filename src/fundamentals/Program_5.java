d65f2dc3d29920568bcdedc2e875dd49cb08a92f
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 18:49:15 +0530

    Done with Program 5

---
+++ src/fundamentals/Program_5.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_5 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("enter your name");
+
+	}
+
+}
+/*
+
+WAP to print “enter your name” on the output screen.
+Output: enter your name
+
+*/
