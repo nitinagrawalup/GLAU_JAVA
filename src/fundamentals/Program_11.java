8b2a6240dc9b6eb6b4972961bb84532b0a55164d
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 19:57:52 +0530

    Done with Program 11

---
+++ src/fundamentals/Program_11.java
@@ -0,0 +1,23 @@
+package fundamentals;
+
+public class Program_11 {
+
+	public static void main(String[] args) {
+		
+		double input1, input2;
+		input1 = 3.14;
+		input2= 5.0;
+		System.out.println(" 3.14 + 5.0 = "+ (input1+input2));
+		
+	}
+
+}
+
+/*
+WAP to declare and initialize two double type variable. Print the result
+of addition of these variables on the output screen
+Hint: double input1 = 3.14;
+	  double input2 = 5.0;
+Output :
+		the sum of two variables = 8.14
+*/
