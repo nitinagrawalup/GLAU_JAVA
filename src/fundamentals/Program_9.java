4dfa313c6c9ab26d0ce6f3d32482e55b1dee567d
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 19:46:49 +0530

    Done with Program 9

---
+++ src/fundamentals/Program_9.java
@@ -0,0 +1,19 @@
+package fundamentals;
+
+public class Program_9 {
+
+	public static void main(String[] args) {
+		
+		double result = 22/7;
+		System.out.println("22/7 = "+ result);
+
+	}
+
+}
+
+/*
+
+WAP to solve the expression :
+22 / 7 ; store the result in a variable and print it on the output screen.
+
+*/
