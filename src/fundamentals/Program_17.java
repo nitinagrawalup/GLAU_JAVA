ef0f32de995ce0932dba375854086381addd6f1b
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 22:06:29 +0530

    Done with Program 17

---
+++ src/fundamentals/Program_17.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Program_17 {
+
+	public static void main(String[] args) {
+		
+		double result = 23 / 10.0;
+		System.out.println("23 / 10.0 = "+ result);
+
+	}
+
+}
+
+/*
+WAP to solve the expression :
+23 / 10.0 ; store the result in a variable and print it on the output screen
+*/
