8560fb9d13f467c1ee9f0be23ef7e40993fe4b34
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 18:41:17 +0530

    Done with Program 2

---
+++ src/fundamentals/Program_2.java
@@ -0,0 +1,19 @@
+package fundamentals;
+
+public class Program_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Good Evening,\nWelcome to Programming");
+
+	}
+
+}
+
+/*
+
+WAP to display “Good Evening, Welcome to Programming” on the
+output screen.
+Output : Good Evening,
+Welcome to Programming
+*/
