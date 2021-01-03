cf57bb19a14509fa840177facbc4bfe227af005f
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-03 18:45:08 +0530

    Done with Program 3

---
+++ src/fundamentals/Program_3.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+public class Program_3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter an integer");
+
+	}
+
+}
+
+/*
+
+WAP to print “enter an integer” on the output screen.
+Output: enter an integer
+
+*/
