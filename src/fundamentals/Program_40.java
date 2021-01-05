148ca77fc32d81e1e65b9975aa46fdaa076b0aef
Nitin Agrawal <nitinagrawalup@gmail.com>
2021-01-05 20:12:52 +0530

    Done with Program 40

---
+++ src/fundamentals/Program_40.java
@@ -0,0 +1,22 @@
+package fundamentals;
+
+public class Program_40 {
+
+	public static void main(String[] args) {
+		
+		display();
+
+	}
+
+	private static void display() {
+		
+		System.out.println("Hello World!!");
+		
+	}
+
+}
+/*
+WAP to display hello world on output screen using a method called
+display, which doesn't accept any arguments and will not return any
+value. This method will print hello world on output screen
+*/
